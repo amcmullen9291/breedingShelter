29c0ab6607a9da2e878fd894994c5b16623c622b
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-17 07:02:27 -0500

    adding [admin]staff actions

---
+++ app/controllers/staffs_controller.rb
@@ -0,0 +1,27 @@
+class StaffsController < ApplicationController
+    before_action: :staff_apt, only: [:show, :edit, destroy] 
+    
+    def index
+        @staffs = Staff.all
+    end 
+
+    def new 
+        @staff = Staff.new 
+    end
+
+    def create 
+    end 
+
+    def show 
+    end
+
+    def edit 
+    end
+
+    def destroy 
+    end
+
+    private 
+
+
+end
